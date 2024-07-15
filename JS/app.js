5bc8801ef3ed6ae28ed9da71444f5d958da3ae52
Админ <macbahodir@gmail.com>
2024-07-15 15:22:12 +0500

    Adding some navigation tags

--- JS/app.js
+++ JS/app.js
@@ -3,3 +3,10 @@ const header = document.querySelector('.header');
 window.addEventListener('scroll', function () {
     header.classList.add('fixed')
 })
+
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
